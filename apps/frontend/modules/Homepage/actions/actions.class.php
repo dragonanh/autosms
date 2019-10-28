957d8977aceb5a7d2a57bfbaae837eb2fb435840
dragonanh <buihuuvuanh@gmail.com>
2019-10-28 15:30:30 +0700

    ok

--- apps/frontend/modules/Homepage/actions/actions.class.php
+++ apps/frontend/modules/Homepage/actions/actions.class.php
@@ -11,7 +11,19 @@
 class HomepageActions extends sfActions
 {
   public function executeIndex(sfWebRequest $request){
-
+    $transId = date('ymdHis').rand(10000,99999);
+    $params = [
+      'SUB' => 'AUTOSMS_DAILY', 'CATE' => 'autosms', 'ITEM' => 'qrcode',
+      'SUB_CP' => 'ghd', 'CONT' => 'qrcode', 'PRICE' => 0,
+      'PRO' => 'GHD', 'SER' => 'AutoSMS', 'REQ' => $transId
+    ];
+    $mps = new MpsWS();
+//    $mpsUrl = $mps->getMpsUrl($params, MpsWS::MOBILE);
+    $data = 'Jsf5isu9JkH1WBvHdZUrXMQtLBVHraXfaqDigLS9QBzCgNypOr2XFT0sbgj05YYYcsnZsk7gntMQ97eYJvj+d77FkxoFEl2CsNUXzkCMhLBMeU2fyVXlt4ERBkzwvUVB4dndQDtuKzS3LbNgBWVsYys5l13/Vi5XphS7mktYPqqx150ZonFQY1q+h61jZwWeaIyHr0DVB2H9t1Zm64FC0J3VsiH8RTCfKG6jtxHOGToV8eKlu2JmyxfxJgMv5TfxOG59MqziBhB8GGkhJUtK3qINyg5REsbmxXBRq/Y/9ejj37XYEiBZbJaeBYaoqVncbu7WJEPwrXajl7mjbgEWTzQaQ2fdiERRTLGyvXk8aXRwyq+Sqqx/gt2CsK27VlneMM3hE+yi54QayVYlwaDuccpgre/boV1bpPrYPqOfoxbJlJSeAhcuAjtCqp1xfzFV8YbCRiSYh1Wi7VuAj9qn7kFL7ZI1gaGeEKrgf2fM0vl5LYK6+Xc1J1h7nAWtCgXXjt5TUyGaH/T/FW1erOkX1P8ehJEF5emyvaqRkX7QPMTzk6+4atYysp6IhX3ue/61g7tWB/pV55XgYotsfJs8Q0cHrBtBPFHXqVEtJTgVDKlE7D0crZro4zu7nuFrvB9zeJmVLxbjR0O4PBjReRzZeTUD+7cWeoeLmBmUiYLGeIE=';
+    $sign = 'JZ5fTh5n15PQEoC/a/N64VLANAx4IAq1gL4Jgso2U2EZQ3ijcqP59nFfPjWv+D2GIC/tL8Wgn4oZZr3U63qNsf3lUOxDsgv+bktrR8T7LsgHkOgd86tgdjiqHmBUPMNFTawZCQKWeVDO7FOWyCrMddm4fc3/Od+qgnHsAh4EPpc3HSiq/ZJ1WFwLoFprBBtwAxauWzVJcXhQ+LIusQDdh5UclRTC/hnszPvU7EPao9bh7Pmo3ZTixiJVQp7k0ARlIXVXGrEwNOM2+LLaB+RyKZyQRTKes4MzQs1EYUEAScBRFkSZc43AOD+7DipCvVWfzYT70+OmPUIhQKSg+pc4aXzwNupNXi9SqD9lDM2jIDFOxo7Ug3argZMXDLVeT5PrxMHTrUklkLJRtud+wmXe6EsWhOPkl7oyqMBOyL+beR42huhIkjNm2LlFnt+0EBLMsVHskWTXiSuzrSuU0Nq0O6mAKclZK8cPWqL8uSHtiqGq6zWvoxikPRvvDGWEBIZodM5wvDGdYvlJk+n+2XmyCxtjvsJmoU/qsjazixNKYTQw0y7tfZtm/smenS+WjtNiXn1AgjAD9qdOxfI0c587lsLOIVMDjP7B1z1VyBc8NhdFqTmznBU69W7SXRrgwSUZIiSvpciL89WtLKiBV9d+MgFTIaWMmVnoLNPoQN9VJfc=' ;
+    $mps->decryptData($data, $sign);
+//    var_dump($mpsUrl);
+    die;
   }
 
   public function executeCreate(sfWebRequest $request){
@@ -48,7 +60,7 @@ class HomepageActions extends sfActions
             'PRO' => 'GHD', 'SER' => 'AutoSMS', 'REQ' => $transId
           ];
           $mps = new MpsWS();
-          $mpsUrl = $mps->getMpsChargeUrl($params);
+          $mpsUrl = $mps->getMpsUrl($params, MpsWS::CHARGE);
           $mpsUrl = $this->generateUrl('mpsResult', ['RES' => 0, 'MOBILE' => '0354926551', 'REQ' => $transId]);
           $this->redirect($mpsUrl);
         }
--- lib/vtLib/webservice/MpsWS.php
+++ lib/vtLib/webservice/MpsWS.php
@@ -10,27 +10,35 @@ class MpsWS
 {
   private $aesKey;
 
+  const CHARGE = 1;
+  const MOBILE = 2;
+
   public function __construct()
   {
     $z = 'abcdefghijuklmno0123456789012345';
     $aes = new AES($z);
     $encrypted = $aes->encrypt();
-    $this->aeskey = bin2hex($encrypted);
+    $this->aesKey = bin2hex($encrypted);
   }
 
-  public function encryptData($params){
+  public function encryptData($params, $type){
     $key_path = sfConfig::get('sf_root_dir').DIRECTORY_SEPARATOR.'data'.DIRECTORY_SEPARATOR.'key';
     $pub_key = file_get_contents($key_path.DIRECTORY_SEPARATOR.'PublicKeyVT.pem');
     $pri_key_cp = file_get_contents($key_path.DIRECTORY_SEPARATOR.'PrivateKeyCP.pem');
 
-    //CP thuc hienng h
-
-    $data = sprintf('SUB=%s&CATE=%s&ITEM=%s&SUB_CP=%s&CONT=%s&PRICE=%s&REQ=%s&SOURCE=WAP',
-      $params['SUB'],$params['CATE'],$params['ITEM'],$params['SUB_CP'],$params['CONT'],$params['PRICE'],$params['REQ']);
+    switch ($type){
+      case self::CHARGE:
+        $data = sprintf('SUB=%s&CATE=%s&ITEM=%s&SUB_CP=%s&CONT=%s&PRICE=%s&REQ=%s&SOURCE=WAP',
+          $params['SUB'],$params['CATE'],$params['ITEM'],$params['SUB_CP'],$params['CONT'],$params['PRICE'],$params['REQ']);
+        break;
+      default:
+        $data = sprintf('SUB=%s&SESS=%s&REQ=%s&SOURCE=WAP',
+          $params['SUB'],session_id(),$params['REQ']);
+    }
 
     $data = $this->pkcs5_pad($data, 16);
     //B1. Ma hoa du lieu bang AES
-    $value_encrypt_aes = $this->encrypt($data,$this->aeskey);
+    $value_encrypt_aes = $this->encrypt($data,$this->aesKey);
 
     //B2. Input du lieu co gan key AES
     $value_with_key = 'value='.$value_encrypt_aes.'&key='.$this->aesKey;
@@ -51,11 +59,34 @@ class MpsWS
     return [$data_encrypted, $signature];
   }
 
-  public function getMpsChargeUrl($params){
-    list($dataEncrypt, $signature) = $this->encryptData($params);
+  public function decryptData($data_encrypted, $signature){
+    $key_path = sfConfig::get('sf_root_dir').DIRECTORY_SEPARATOR.'data'.DIRECTORY_SEPARATOR.'key';
+    $pub_key_cp = file_get_contents($key_path.DIRECTORY_SEPARATOR.'PublicKeyVT.pem');
+    $pri_key_cp = file_get_contents($key_path.DIRECTORY_SEPARATOR.'PrivateKeyCP.pem');
+
+    $verify = openssl_verify (($data_encrypted) , base64_decode(str_replace(' ', '+',urldecode($signature))) , $pub_key_cp, OPENSSL_ALGO_SHA1);
+    if($verify) {
+      //B6. Giai ma du lieu bang private key
+      openssl_private_decrypt(base64_decode(str_replace(' ', '+', urldecode($data_encrypted))), $data_decrypted, $pri_key_cp);
+      $arr = array();
+      parse_str($data_decrypted, $arr);
+
+      //B7. Giai ma du lieu bang AES
+      $value_decrypt = $this->decrypt(str_replace(' ', '+', $arr['VALUE']), $arr['KEY']);
+    }
+  }
+
+  public function getMpsUrl($params, $type){
+    list($dataEncrypt, $signature) = $this->encryptData($params, $type);
     $server = sfConfig::get('app_mps_url');
     // điền các thông tin của dịch vụ
-    return $server.'charge.html?PRO=GHD&CMD=DOWNLOAD&SER=AutoSMS&SUB=AUTOSMS_DAILY&DATA='.urlencode( $dataEncrypt).'&SIG='.$signature;
+    switch ($type){
+      case self::CHARGE:
+        return $server.'charge.html?PRO=GHD&CMD=DOWNLOAD&SER=AutoSMS&SUB=AUTOSMS_DAILY&DATA='.urlencode( $dataEncrypt).'&SIG='.$signature;
+      default:
+        return $server.'mobile.html?PRO=GHD&SER=AutoSMS&SUB=AUTOSMS_DAILY&DATA='.urlencode( $dataEncrypt).'&SIG='.$signature;
+    }
+
   }
 
   protected function encrypt($encrypt, $key){
